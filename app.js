1e906b309d69fa2660cfa2612feb7baf35047245
sunnyfur <polin.makarova@mail.ru>
2022-10-31 16:26:29 +0500

    refactor: add class to checkbox according to rule 3.4 in html-and-css.md and 2.1 in http://html-and-css-extended.md

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList="task__checkbox";
     editInput.type="text";
     editInput.className="task__text task__text_edit";
 
