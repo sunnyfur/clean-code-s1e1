728c58a4aa588dbe9df8d309b5a2f1a7b14c76e1
sunnyfur <polin.makarova@mail.ru>
2022-10-31 16:42:45 +0500

    refactor: add class button__img according to rules 3.4 in html-and-css.md and 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="button__img";
     deleteButton.appendChild(deleteButtonImg);
 
 
